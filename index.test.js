9bb7c4b1f1aace97cf83efe26d96d520828d6d3b
Dair Smith <dair.code@gmail.com>
2021-04-27 16:00:41 -0400

    tests for consonants

--- index.test.js
+++ index.test.js
@@ -17,9 +17,19 @@ describe("translate function", () => {
         expect(result).toEqual("argonway");
     });
 
-    test("start with vowel", () => {
+    test("start with vowel capital", () => {
         let result = translate("Argon");
         expect(result).toEqual("argonway");
+    });
+
+    test("starts with consonants", () => {
+        let result = translate("tree");
+        expect(result).toEqual("eetray");
+    });
+
+    test("starts with consonants capital", () => {
+        let result = translate("Tree");
+        expect(result).toEqual("eetray");
     });
 });
 
